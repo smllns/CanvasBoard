bd0be08e1ed0e8845fba4ed448748adac68623ed
smllns <marysmoly@gmail.com>
2024-07-08 15:34:51 +0300

    morec ode clearing

--- lib/useMaxZIndex.ts
+++ lib/useMaxZIndex.ts
@@ -1,13 +1,11 @@
-import { useMemo } from "react";
+import { useMemo } from 'react';
 
-import { useThreads } from "@/liveblocks.config";
+import { useThreads } from '@/liveblocks.config';
 
 // Returns the highest z-index of all threads
 export const useMaxZIndex = () => {
-  // get all threads
   const { threads } = useThreads();
 
-  // calculate the max z-index
   return useMemo(() => {
     let max = 0;
     for (const thread of threads) {
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,3 @@
-import jsPDF from 'jspdf';
 import { twMerge } from 'tailwind-merge';
 import { type ClassValue, clsx } from 'clsx';
 
@@ -97,26 +96,3 @@ export const getShapeInfo = (shapeType: string) => {
       };
   }
 };
-
-// export const exportToPdf = () => {
-//   const canvas = document.querySelector('canvas');
-//   console.log(canvas);
-
-//   if (!canvas) return;
-
-//   // use jspdf
-//   const doc = new jsPDF({
-//     orientation: 'landscape',
-//     unit: 'px',
-//     format: [canvas.width, canvas.height],
-//   });
-
-//   // get the canvas data url
-//   const data = canvas.toDataURL();
-
-//   // add the image to the pdf
-//   doc.addImage(data, 'PNG', 0, 0, canvas.width, canvas.height);
-
-//   // download the pdf
-//   doc.save('canvas.pdf');
-// };
